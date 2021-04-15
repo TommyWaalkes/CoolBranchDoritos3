15d165eb3bb5f2df2eac3640557c6d539fd78b92
Tommy Waalkes <Tommy@grandcircus.co>
2021-04-15 11:16:27 -0400

    Added Pancakes

--- BranchTest/Program.cs
+++ BranchTest/Program.cs
@@ -7,6 +7,7 @@ namespace BranchTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Maple syrup and pancakes are good!");
         }
     }
 }
